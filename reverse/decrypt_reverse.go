92bf0c789dafb4f14c64cec7ba74305c88a04805
mr.curdy@gmail.com <mr.curdy@gmail.com>
2024-08-10 16:45:19 +0300

    Final adjustments

--- reverse/decrypt_reverse.go
+++ reverse/decrypt_reverse.go
@@ -10,16 +10,3 @@ func Decrypt_reverse(s string) string {
 	return string(reversedRunes)
 
 }
-
-// func ReverseAlphabet2(ch rune) rune {
-// 	var reverse rune
-// 	if ch >= 'a' && ch <= 'z' {
-// 		reverse = 'z' - (ch - 'a')
-// 	} else if ch >= 'A' && ch <= 'Z' {
-// 		reverse = 'Z' - (ch - 'A')
-// 	} else {
-// 		return ch
-// 	}
-
-// 	return reverse
-// }
--- reverse/encript_reverse.go
+++ reverse/encript_reverse.go
@@ -10,18 +10,3 @@ func Encrypt_reverse(s string) string {
 	return string(reversedRunes)
 
 }
-
-// func ReverseAlphabet(ch rune) rune {
-// 	var reverse rune
-// 	if ch >= 'a' && ch <= 'z' {
-// 		reverse = 'z' - (ch - 'a')
-// 	} else if ch >= 'A' && ch <= 'Z' {
-// 		reverse = 'Z' - (ch - 'A')
-// 	} else {
-// 		return ch
-// 	}
-
-// 	return reverse
-// }
-
-// func encrypt_reverse(s string) string {}
