1be992ab80978c1e41c9a2ceecf3e791e7445c10
mr.curdy@gmail.com <mr.curdy@gmail.com>
2024-08-10 14:51:00 +0300

    reverse alphabet func optimisation

--- reverse/decrypt_reverse.go
+++ reverse/decrypt_reverse.go
@@ -2,23 +2,24 @@ package reverse
 
 func Decrypt_reverse(s string) string {
 	reversedRunes := make([]rune, len(s))
-	
+
 	for i, r := range s {
-		reversedRunes[len(s)-1-i] = ReverseAlphabet2(r)
+		reversedRunes[len(s)-1-i] = ReverseAlphabet(r)
 	}
-	
+
 	return string(reversedRunes)
-	
+
 }
-func ReverseAlphabet2(ch rune) rune {
-	var reverse rune
-	if ch >= 'a' && ch <= 'z' {
-		reverse = 'z' - (ch - 'a')
-	} else if ch >= 'A' && ch <= 'Z' {
-		reverse = 'Z' - (ch - 'A')
-	} else {
-		return ch
-	}
-	
-	return reverse 
-}
+
+// func ReverseAlphabet2(ch rune) rune {
+// 	var reverse rune
+// 	if ch >= 'a' && ch <= 'z' {
+// 		reverse = 'z' - (ch - 'a')
+// 	} else if ch >= 'A' && ch <= 'Z' {
+// 		reverse = 'Z' - (ch - 'A')
+// 	} else {
+// 		return ch
+// 	}
+
+// 	return reverse
+// }
