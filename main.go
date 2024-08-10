f769dd100c75251bc9af0008e8878f211e3ce6bd
Marian <mariandelgado158@gmail.com>
2024-08-10 12:42:51 +0300

    encriptreverse done

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
+import (
+	"cypher/reverse"
 
+	"fmt"
+)
 func main() {
-
+ 	a := reverse.Encrypt_reverse("Abc, Marian")
+	fmt.Println(a)
 }
--- reverse/encript_reverse.go
+++ reverse/encript_reverse.go
@@ -1,1 +1,25 @@
 package reverse
+
+
+func Encrypt_reverse(s string) string {
+	reversedRunes := make([]rune, len(s))
+	
+	for i, r := range s {
+		reversedRunes[len(s)-1-i] = reverseAlphabetValue(r)
+	}
+	
+	return string(reversedRunes)
+	
+}
+func reverseAlphabetValue(ch rune) rune {
+	var reverse rune
+	if ch >= 'a' && ch <= 'z' {
+		reverse = 'z' - (ch - 'a')
+	} else if ch >= 'A' && ch <= 'Z' {
+		reverse = 'Z' - (ch - 'A')
+	} else {
+		return ch
+	}
+	
+	return reverse
+}
