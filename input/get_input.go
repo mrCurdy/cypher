09bae4863f852016321a3a6563b02b4852829abf
mr.curdy@gmail.com <mr.curdy@gmail.com>
2024-08-10 19:39:02 +0300

    variables added to carry strings

--- input/get_input.go
+++ input/get_input.go
@@ -8,11 +8,17 @@ func GetInput() (toEncrypt bool, encoding string, message string) {
 
 	falseInputs := true
 
-	fmt.Print("Welcome to the Cypher Tool!\n\nSelect operation (1/2):\n")
+	welcomeSign := "Welcome to the Cypher Tool!\n\nSelect operation (1/2):\n"
+	encDecMenu := "1. Encrypt.\n2. Decrypt."
+	wrongChoiceSign := "\nSelect correct operation 1 or 2\n"
+	cypherSelectMenu := "\nSelect cypher (1/3):\n1. ROT13.\n2. Reverse.\n3. Reverse message and shift"
+	entMessageSign := "\nEnter the message:"
+
+	fmt.Print(welcomeSign)
 
 	for falseInputs {
 		var i int
-		fmt.Println("1. Encrypt.\n2. Decrypt.")
+		fmt.Println(encDecMenu)
 		fmt.Scanln(&i)
 
 		switch i {
@@ -21,16 +27,16 @@ func GetInput() (toEncrypt bool, encoding string, message string) {
 		case 2:
 			toEncrypt = false
 		default:
-			fmt.Print("\nSelect correct operation 1 or 2\n")
+			fmt.Print(wrongChoiceSign)
 			continue
 		}
 
-		fmt.Println("\nSelect cypher (1/3):\n1. ROT13.\n2. Reverse.\n3. Reverse message and shift")
-		fmt.Scanln(&encoding)
+		fmt.Println(cypherSelectMenu)
+		encoding = Scanner()
 
 		if encoding == "1" || encoding == "2" || encoding == "3" {
 
-			fmt.Println("\nEnter the message:")
+			fmt.Println(entMessageSign)
 			message = Scanner()
 			fmt.Println()
 			falseInputs = false
