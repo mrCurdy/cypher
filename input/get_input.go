85a3dd93229aaa43b817ff5f0fbdaefa3bb6de34
Albert D <a.derevski@outlook.com>
2024-08-10 15:31:55 +0300

    Final

--- input/get_input.go
+++ input/get_input.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-func getInput() (toEncrypt bool, encoding string, message string) {
-	correctInputs := false
-	for correctInputs {
+func GetInput() (toEncrypt bool, encoding string, message string) {
+	falseInputs := true
+	for falseInputs {
 		var i int
 		fmt.Print("Welcome to the Cypher Tool!\n\nSelect operation (1/2):\n1. Encrypt.\n2. Decrypt.\n")
 		fmt.Scanln(&i)
@@ -20,12 +20,14 @@ func getInput() (toEncrypt bool, encoding string, message string) {
 			fmt.Print("Select correct operation 1 or 2")
 			continue
 		}
-		fmt.Println("Select cypher (1/2):\n1. ROT13.\n2. Reverse.\n3. Reverse message and shift")
+		fmt.Println("Select cypher (1/3):\n1. ROT13.\n2. Reverse.\n3. Reverse message and shift")
 		fmt.Scanln(&encoding)
 		fmt.Println()
 		if encoding == "1" || encoding == "2" || encoding == "3" {
 			fmt.Println("Enter the message:")
 			fmt.Scanln(&message)
+			fmt.Println()
+			falseInputs = false
 		} else {
 			continue
 		}
