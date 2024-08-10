d63ded7051637c7a70b51fac34b047567ecaf99f
Albert D <a.derevski@outlook.com>
2024-08-10 15:06:07 +0300

    dfdf

--- input/get_input.go
+++ input/get_input.go
@@ -20,10 +20,10 @@ func getInput() (toEncrypt bool, encoding string, message string) {
 			fmt.Print("Select correct operation 1 or 2")
 			continue
 		}
-		fmt.Println("Select cypher (1/2):\n1. ROT13.\n2. Reverse.")
+		fmt.Println("Select cypher (1/2):\n1. ROT13.\n2. Reverse.\n3. Reverse message and shift")
 		fmt.Scanln(&encoding)
 		fmt.Println()
-		if encoding == "1" || encoding == "2" {
+		if encoding == "1" || encoding == "2" || encoding == "3" {
 			fmt.Println("Enter the message:")
 			fmt.Scanln(&message)
 		} else {
