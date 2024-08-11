8637974efa224f7a445cc08b9d0c3f57fc82c6b1
mr.curdy@gmail.com <mr.curdy@gmail.com>
2024-08-11 10:55:42 +0300

    comments deleted

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-// 	"cypher/input"
-// 	"cypher/reverse"
-// 	"fmt"
-// )
-
 func main() {
 	// This is our function!!!
 	cypher()
-
-	// This is testing
-	// a := "Hello, my friend!"
-
-	// b := input.TrimSpaces(a)
-
-	// c := reverse.Encrypt_reverse(b)
-
-	// fmt.Println(c)
-
-	// Social media videos of the crash showed the plane spiraling out of the sky before hitting the ground as people in the neighborhood shouted in fear. Another video showed the wreckage of the plane in flames on the ground. No one on the ground was hurt, city officials told CNN.
 
 }
